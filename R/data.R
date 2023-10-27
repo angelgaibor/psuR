90735cd5a5950be6870cd370028732cbd779a27d
desarrollador-endi1 <103216712+desarrollador-endi1@users.noreply.github.com>
2023-10-27 12:58:24 -0500

    Subida de paquete psuR

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Dataset with number of household in every block
+#'
+#' @format A data frame with 249 PSU rows and 2 variables:
+#' \describe{
+#'   \item{id}{id of PSU}
+#'   \item{viv}{Number of households in every household}
+#'
+#' }
+#'
+#' @source {Created as example of weights}
+#'
+#' @example
+#' data(df_weights)
+"df_weights"
